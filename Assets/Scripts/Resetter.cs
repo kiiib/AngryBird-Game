4a787ba8206281d4aaa0a7bd98f514b2b0291122
Jian-Bin Liu <billyljb@gmail.com>
2017-03-27 18:19:23 +0800

    update

---
+++ Assets/Scripts/Resetter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resetter : MonoBehaviour {
+    public Rigidbody2D projectile;
+    public float resetSpeed = 0.025f;
+
+    private float resetSpeedSqr;
+    private SpringJoint2D spring;
+
+	// Use this for initialization
+	void Start () {
+        resetSpeedSqr = resetSpeed * resetSpeed;
+        spring = projectile.GetComponent<SpringJoint2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Reset();
+        }
+
+        if(spring == null && projectile.velocity.sqrMagnitude < resetSpeedSqr) {
+            Reset();
+        }
+	}
+
+    void OntriggerExit2D(Collider2D other)
+    {
+        if(other.GetComponent<Rigidbody2D>() == projectile) {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
